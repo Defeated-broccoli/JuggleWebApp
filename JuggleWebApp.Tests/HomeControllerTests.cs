4c5d98eabd5913e3a0a5260118cee42114c9948d
Piotr Kwietniewski <wormx712@gmail.com>
2023-05-26 16:49:13 +0200

    Added HomeControllerTests to JuggleWebApp.Tests. Added dependencies.

---
+++ JuggleWebApp.Tests/HomeControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using FakeItEasy;
+using JuggleWebApp.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuggleWebApp.Tests
+{
+    public class HomeControllerTests
+    {
+        private IPostRepository _postRepository;
+        private ITournamentRepository _tournamentRepository;
+        private ICommentRepository _commentRepository;
+        private IBlobStorageService _blobStorageService;
+        private IHttpContextAccessor _httpContextAccessor;
+
+        public HomeControllerTests()
+        {
+            //Dependencies
+            _postRepository = A.Fake<IPostRepository>();
+            _tournamentRepository = A.Fake<ITournamentRepository>();
+            _commentRepository = A.Fake<ICommentRepository>();
+
+            _blobStorageService = A.Fake<IBlobStorageService>();
+            _httpContextAccessor = A.Fake<IHttpContextAccessor>();
+        }
+    }
+}
---
+++ JuggleWebApp.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace JuggleWebApp.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
